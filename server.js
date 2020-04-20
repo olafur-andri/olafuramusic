065e79a4141be1b1ff193a3d0f782dcf60077796
olafur-andri <olafur1199@gmail.com>
2020-04-20 10:55:25 +0000

    Fixed linting and testing steps

--- server.js
+++ server.js
@@ -8,10 +8,11 @@ app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-app.get('/*', function (req, res) {
+
+app.get('/ping', (req, res) => res.send('pong'));
+
+app.get('/*', (_, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
 app.listen(port);
---
+++ src/components/App/App.test.js
@@ -0,0 +1,5 @@
+describe('<App />', () => {
+  it('Should say that 1 === 1', () => {
+    expect(1).toBe(1);
+  });
+});
