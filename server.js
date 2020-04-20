6dc7c0bb8bdd63e709d9ec268fc63b700339bd7c
olafur-andri <olafur1199@gmail.com>
2020-04-20 15:40:42 +0000

    Removed /ping path

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', (req, res) => res.send('pong'));
-
 app.get('/*', (_, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
